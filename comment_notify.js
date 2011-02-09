700ad786c79d0f744031f38c870ddcbbba03ed6e
Greg (greggles) Knaddison <greg@growingventuresolutions.com>
2011-02-09 12:31:35 +0000

    bug #802284 by ksenzee: javascript was broken and old style

--- comment_notify.js
+++ comment_notify.js
@@ -1,17 +1,20 @@
 // $Id$
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    $("#edit-notify-type-1").bind("click", function() {
-      if ($("#edit-notify").attr("checked", false)) {
-        // Auto-notification not checked - do it for them.
-        $("#edit-notify").attr("checked",true);
-      }
-    });
-    $("#edit-notify-type-2").bind("click", function() {
-      if ($("#edit-notify").attr("checked", false)) {
-        // Auto-notification not checked - do it for them.
-        $("#edit-notify").attr("checked",true);
-      }
-    });
-  });
-}
+(function ($) {
+  Drupal.behaviors.commentNotify = {
+    attach: function (context) {
+      $('#edit-notify-type', context).hide();
+      $('#edit-notify', context).bind('change', function() {
+        if ($(this).attr('checked')) {
+          $('#edit-notify-type', context).show();
+          if ($('#edit-notify-type input:checked', context).length == 0) {
+            $('#edit-notify-type input', context)[0].checked = 'checked';
+          }
+        }
+        else {
+          $('#edit-notify-type', context).hide();
+        }
+      });
+      $('#edit-notify', context).trigger('change');
+    }
+  }
+})(jQuery);
